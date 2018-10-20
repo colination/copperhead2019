d9b1d7ec126a3b91f638ea2168585c991df1582b
rohitalamgari <rohit.alamgari@gmail.com>
2018-10-20 18:18:07 -0500

    test file for gold in CV

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/inchesTestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/inchesTestAuto.java
@@ -9,10 +9,16 @@ public class inchesTestAuto extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap,telemetry);
+        telemetry.addData("ticks", robot.driveTrain.mtrBL.getCurrentPosition());
+        telemetry.update();
         boolean finished = false;
         waitForStart();
         while (opModeIsActive() && finished == false){
+<<<<<<< HEAD
             robot.driveTrain.goInches(12,.5,6);
+=======
+            robot.driveTrain.goInches(24.0, .2, 6);
+>>>>>>> 75fd468f4c2cef12c55c29a0f0ef1931b55db4c5
             robot.driveTrain.stopMotors();
             finished = true;
         }
