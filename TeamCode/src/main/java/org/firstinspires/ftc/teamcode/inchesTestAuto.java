08fd863e44705b0c14a8ac4442411a10befbbbc8
Jabitzen <inventedbaton7@gmail.com>
2018-11-03 15:02:52 -0500

    android 4.3 upda

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/inchesTestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/inchesTestAuto.java
@@ -1,25 +1,22 @@
 package org.firstinspires.ftc.teamcode;
-
-import com.disnodeteam.dogecv.CameraViewDisplay;
-import com.disnodeteam.dogecv.DogeCV;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name = "Inch Test", group = "12596")
 public class inchesTestAuto extends LinearOpMode {
     CopperHeadRobot robot = new CopperHeadRobot();
-    private TestCV detector;
+    //private TestCV detector;
     @Override
     public void runOpMode() throws InterruptedException {
-        detector = new TestCV();
-        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
-        detector.useDefaults();
-        detector.downscale = 0.4; // How much to downscale the input frames
-        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
-        detector.maxAreaScorer.weight = 0.005;
-        detector.ratioScorer.weight = 5;
-        detector.ratioScorer.perfectRatio = 1.0;
-        detector.enable();
+        //detector = new TestCV();
+        //detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        //detector.useDefaults();
+        //detector.downscale = 0.4; // How much to downscale the input frames
+        //detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+        //detector.maxAreaScorer.weight = 0.005;
+        //detector.ratioScorer.weight = 5;
+        //detector.ratioScorer.perfectRatio = 1.0;
+        //detector.enable();
 
         robot.init(hardwareMap,telemetry);
         telemetry.addData("ticks", robot.driveTrain.mtrBL.getCurrentPosition());
