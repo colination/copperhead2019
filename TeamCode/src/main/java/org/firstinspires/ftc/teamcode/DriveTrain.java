f62fcc2aa1671805a22434eae1fa32b3e2938094
Jabitzen <inventedbaton7@gmail.com>
2018-11-07 17:08:58 -0600

    Update TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -8,17 +8,6 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> d1efc57b75f31e385c1841bdecc9de468559dd9f
->>>>>>> 69159988f1070be542823483c6dcc3ec1fcc260c
->>>>>>> 1ab03f2d0e6646be292a7cda6c7017c9112796d6
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -41,21 +30,11 @@ public class DriveTrain extends robotPart {
 
     //servos
     public Servo srvRoller = null;
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
     //BNO055IMU               imu;
     //Orientation             lastAngles = new Orientation();
     //double globalAngle, power = .30, correction;
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 69159988f1070be542823483c6dcc3ec1fcc260c
 
-
->>>>>>> d1efc57b75f31e385c1841bdecc9de468559dd9f
->>>>>>> 1ab03f2d0e6646be292a7cda6c7017c9112796d6
     public ElapsedTime runtime = new ElapsedTime();
     double     COUNTS_PER_MOTOR_REV    = 1120 ;
     double     DRIVE_GEAR_REDUCTION    = .5 ;
@@ -90,28 +69,6 @@ public class DriveTrain extends robotPart {
 
         //Servos
         srvRoller = ahwmap.servo.get("srvRoller");
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-        srvRoller.setPosition(0.4);
-
-=======
-<<<<<<< HEAD
->>>>>>> 1ab03f2d0e6646be292a7cda6c7017c9112796d6
-
-        srvRoller.setPosition(0.4);
-
-        srvRoller.setPosition(0);
-
-<<<<<<< HEAD
-=======
-=======
-        srvRoller.setPosition(0.4);
->>>>>>> d1efc57b75f31e385c1841bdecc9de468559dd9f
->>>>>>> 69159988f1070be542823483c6dcc3ec1fcc260c
->>>>>>> 1ab03f2d0e6646be292a7cda6c7017c9112796d6
-
         srvRoller.setPosition(0.4);
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -250,12 +207,12 @@ public class DriveTrain extends robotPart {
     }
     public void setSideRoller(double Position) {
         srvRoller.setPosition(Position);
+    }
 
-        }
     public void gyroInches(double inches, double power) {
         reset();
 
-<<<<<<< HEAD
+
         mtrFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         mtrFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         mtrBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -297,65 +254,13 @@ public class DriveTrain extends robotPart {
                 }
                 stopMotors();
             }
-
-
-                public void gyroInches ( double inches){
+            public void gyroInches ( double inches){
                     reset();
                     setMode();
                     targetPosition(inches);
 
-                }
-
-                public void resetAngle ()
-                {
-                    lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-
-                    globalAngle = 0;
-                }
-                public double getAngle ()
-                {
-                    // We experimentally determined the Z axis is the axis we want to use for heading angle.
-                    // We have to process the angle because the imu works in euler angles so the Z axis is
-                    // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
-                    // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
-
-                    Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-
-                    double deltaAngle = angles.thirdAngle - lastAngles.thirdAngle;
-
-                    if (deltaAngle < -180)
-                        deltaAngle += 360;
-                    else if (deltaAngle > 180)
-                        deltaAngle -= 360;
-
-                    globalAngle += deltaAngle;
-
-                    lastAngles = angles;
-
-                    return globalAngle;
-                }
-                public double checkDirection ()
-                {
-                    // The gain value determines how sensitive the correction is to direction changes.
-                    // You will have to experiment with your robot to get small smooth direction changes
-                    // to stay on a straight line.
-                    double correction, angle, gain = .10;
-
-                    angle = getAngle();
-
-                    if (angle == 0)
-                        correction = 0;             // no adjustment.
-                    else
-                        correction = -angle;        // reverse sign of angle for correction.
-
-                    correction = correction * gain;
-
-                    return correction;
-                }
-
-            }
-=======
     }
+
 
     public void resetAngle()
     {
@@ -404,7 +309,7 @@ public class DriveTrain extends robotPart {
 
     }
 
-    /*public void rotate(int degrees, double power)
+    public void rotate(int degrees, double power)
     {
         double  leftPower, rightPower;
 
@@ -452,60 +357,8 @@ public class DriveTrain extends robotPart {
 
         // reset angle tracking on new heading.
         resetAngle();
-    }*/
-
-
-
-//    public void rotate(int degrees, double power){
-//            double leftPower = 0, rightPower = 0;
-//
-//            // restart imu movement tracking.
-//            resetAngle();
-//            privateTelemetry.addLine().addData("Robot Angle", getAngle());
-//
-//            // getAngle() returns + when rotating counter clockwise (left) and - when rotating
-//            // clockwise (right).
-//
-//            if (degrees < 0) {   // turn right.
-//                leftPower = -power;
-//                rightPower = power;
-//            } else if (degrees > 0) {   // turn left.
-//                leftPower = power;
-//                rightPower = -power;
-//            }
-//
-//
-//        // set power to rotate.
-//        mtrFL.setPower(leftPower);
-//        mtrBL.setPower(leftPower);
-//        mtrFR.setPower(rightPower);
-//        mtrBR.setPower(rightPower);
-//
-//        // rotate until turn is completed.
-//        if (degrees < 0)
-//        {
-//            // On right turn we have to get off zero first.
-//            while (getAngle() == 0) {}
-//
-//            while (getAngle() > degrees) {}
-//        }
-//        else    // left turn.
-//            while (getAngle() < degrees) {}
-//
-//        // turn the motors off.
-//        stopMotors();
-//
-//        // wait for rotation to stop.
-//        sleep(1000);
-//
-//        // reset angle tracking on new heading.
-//        resetAngle();
-//    }
+    }
+    
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> d1efc57b75f31e385c1841bdecc9de468559dd9f
-}
->>>>>>> 69159988f1070be542823483c6dcc3ec1fcc260c
->>>>>>> 1ab03f2d0e6646be292a7cda6c7017c9112796d6
+
